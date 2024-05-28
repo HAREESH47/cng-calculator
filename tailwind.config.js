ed8fc022e61587b0c48b6f972fade519c37d344e
HAREESH47 <mhareesh120@gmail.com>
2024-05-28 11:59:42 +0530

    button update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "darkpurple": '#481380'
+      }
+    },
   },
   plugins: [],
 }
